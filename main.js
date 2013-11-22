7f02700f8228c87ec7c1ba78c20c18a40b98596c
Lukas Knoepfel <shylux@gmail.com>
2013-11-22 14:07:46 +0100

    Coded everything.

---
+++ main.js
@@ -0,0 +1,8 @@
+$(function() {
+  var test = $('textarea[name="code"').get(0);
+  CodeMirror.fromTextArea(test, {
+    lineNumbers: true,
+    matchBrackets: true,
+    mode: "text/x-java"
+  });
+});
