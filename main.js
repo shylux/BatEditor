2636193c64975062598ad3cbc73395bf8c4c2db5
Lukas Knoepfel <shylux@gmail.com>
2013-11-27 00:12:00 +0100

    Added second theme. Fixed bug where textarea was not synced.

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 $(function() {
-  var test = $('textarea[name="code"').get(0);
-  CodeMirror.fromTextArea(test, {
+  // initialize codemirror
+  var textarea = $('textarea[name="code"').get(0);
+  var cm = CodeMirror.fromTextArea(textarea, {
     lineNumbers: true,
     matchBrackets: true,
-    mode: "text/x-java"
+    mode: "text/x-java",
+	theme: "solarized dark"
   });
+  
+  // sync codemirror and original textarea
+  cm.on("change", function(instance, changeObj) {
+	$('textarea[name="code"').val(cm.getValue());
+  });
+  
+  // change to alternative theme
+  document.onkeydown = function(event) {
+	// CTRL + ALT + SHIFT + M
+	if (event.which == 77 && event.shiftKey && event.ctrlKey && event.altKey) {
+		var otherTheme = (cm.options.theme == "solarized dark") ? "the-matrix" : "solarized dark";
+		cm.setOption("theme", otherTheme);
+	}
+  }
 });
