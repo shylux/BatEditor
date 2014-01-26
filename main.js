baaf41d362024409fd5d69eabfaa305638347dbc
Lukas Knöpfel <shylux@gmail.com>
2014-01-26 15:48:34 +0100

    Added all themes and theme chooser.

--- main.js
+++ main.js
@@ -1,24 +1,65 @@
 $(function() {
   // initialize codemirror
   var textarea = $('textarea[name="code"').get(0);
-  var cm = CodeMirror.fromTextArea(textarea, {
+  
+  cm = CodeMirror.fromTextArea(textarea, {
     lineNumbers: true,
     matchBrackets: true,
     mode: "text/x-java",
-	theme: "solarized dark"
+    theme: "eclipse"
   });
+
+  $(textarea).after(theme_chooser);
+  if (localStorage.getItem("bateditor_theme") != null) {
+    var theme = localStorage.getItem("bateditor_theme");
+    cm.setOption("theme", theme);
+    $("#bateditor_select").val(theme);
+  }
   
+  $("#bateditor_select").on("change", function () {
+    var theme = $("#bateditor_select option:selected").text(); 
+    cm.setOption("theme", theme);
+    localStorage.setItem("bateditor_theme", theme);
+  });
+
   // sync codemirror and original textarea
   cm.on("change", function(instance, changeObj) {
-	$('textarea[name="code"').val(cm.getValue());
+    $('textarea[name="code"').val(cm.getValue());
   });
-  
-  // change to alternative theme
-  document.onkeydown = function(event) {
-	// CTRL + ALT + SHIFT + M
-	if (event.which == 77 && event.shiftKey && event.ctrlKey && event.altKey) {
-		var otherTheme = (cm.options.theme == "solarized dark") ? "the-matrix" : "solarized dark";
-		cm.setOption("theme", otherTheme);
-	}
-  }
+
 });
+
+var theme_chooser = '\
+<p>Select a theme: <select id="bateditor_select">\
+    <option>3024-day</option>\
+    <option>3024-night</option>\
+    <option>ambiance</option>\
+    <option>base16-dark</option>\
+    <option>base16-light</option>\
+    <option>blackboard</option>\
+    <option>cobalt</option>\
+    <option selected>eclipse</option>\
+    <option>elegant</option>\
+    <option>erlang-dark</option>\
+    <option>lesser-dark</option>\
+    <option>mbo</option>\
+    <option>mdn-like</option>\
+    <option>midnight</option>\
+    <option>monokai</option>\
+    <option>neat</option>\
+    <option>night</option>\
+    <option>paraiso-dark</option>\
+    <option>paraiso-light</option>\
+    <option>pastel-on-dark</option>\
+    <option>rubyblue</option>\
+    <option>solarized dark</option>\
+    <option>solarized light</option>\
+    <option>the-matrix</option>\
+    <option>tomorrow-night-eighties</option>\
+    <option>twilight</option>\
+    <option>vibrant-ink</option>\
+    <option>xq-dark</option>\
+    <option>xq-light</option>\
+</select>\
+</p>\
+';
